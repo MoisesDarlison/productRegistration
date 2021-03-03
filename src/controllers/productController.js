ebbedc8a822fcba2f7ca1f63016dcbf12a5ef23c
Darlison <Darlison001@gmail.com>
2021-03-03 16:16:18 -0300

    add function Find by or by title of product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,9 +4,9 @@ const categoryModel = require('../models/category');
 module.exports = {
 
     async create(req, res) {
-       
+
         const { title, description, price, category } = req.body;
-       
+
         try {
             const productAlreadExists = await productModel.findOne({ title });
 
@@ -23,14 +23,14 @@ module.exports = {
 
             const productRegistred = await productModel.create({ title, description, price, assingTo: categoryObject.id });
 
-            return res.status(201).json({productRegistred })
+            return res.status(201).json({ productRegistred })
         } catch (error) {
 
             return res.status(400).json({ message: error.message })
         }
     },
     async index(req, res) {
-       
+
         try {
             const productList = await productModel.find(); //.populate('assingTo')
 
@@ -57,12 +57,12 @@ module.exports = {
             let categoryObject = await categoryModel.findOne({ name: category })
 
             if (!categoryObject) {
-                
+
                 categoryObject = await categoryModel.create({ name: category });
-                
+
             }
 
-           const product =  await productModel.updateOne({ _id: IdProduct },
+            const product = await productModel.updateOne({ _id: IdProduct },
                 {
                     $set: {
                         title,
@@ -74,8 +74,33 @@ module.exports = {
 
             return res.status(201).json({ mesage: "Edit Sucess" })
         } catch (error) {
-            
+
             console.log(error);
+            return res.status(400).json({ message: error.message })
+        }
+    },
+    async filter(req, res) {
+
+        const { IdProduct } = req.params;
+        const { title } = req.body;
+
+        try {
+            let product
+
+            if (IdProduct) {
+                product = await productModel.findOne({ _id: IdProduct });
+            }
+            if (title) {
+                product = await productModel.findOne({ title });
+            }
+
+            if (!product) {
+                return res.status(400).json({ message: "Product does not exists" })
+            }
+
+            return res.status(200).json({ product })
+        } catch (error) {
+
             return res.status(400).json({ message: error.message })
         }
     },
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const route = express();
 
-const { create, index , update} = require('../controllers/productController')
+const { create, index, update, filter } = require('../controllers/productController')
 
 route.post('/', create);
 route.get('/', index);
-route.put('/:IdProduct',update);
-
-
+route.get('/title',filter);
+route.get('/:IdProduct', filter);
+route.put('/:IdProduct', update);
 
 module.exports = route
